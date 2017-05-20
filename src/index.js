aeb138d50156889978401fe69ef27b4072bfb87a
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-05-20 20:40:35 +0200

    fix init right menu

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import 'font-awesome/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
+import 'perfect-scrollbar/dist/js/perfect-scrollbar.min.js';
+
+
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
