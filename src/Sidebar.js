b33adafd0153777107b76ad11391c14b6b1f5aa0
Carlo Di Maio <carlo.dimaio@gmail.com>
2017-05-27 16:08:34 +0200

    add action handler

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,12 +2,82 @@ import React, { Component } from 'react';
 
 import { NavLink } from 'react-router-dom'
 
-// import './App.css';
+
+
+class NavItem extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        // Assign all the correct event handlers.
+        this.setActive = this.setActive.bind(this);
+
+        this.state = {
+            active: props.activeItem ? props.activeItem == props.itemId : false
+        };
+
+
+        console.log('============== NavItem =============');
+        console.log(this.props);
+        console.log(this.state);
+        console.log('====================================');
+
+
+    }
+
+    setActive() {
+        this.setState({ active: true });
+    }
+
+    render() {
+
+        if (this.state.active) {
+            { var tabClassName = "active"; }
+        }
+
+        return (
+            <li className={this.state.active ? 'active' : ''}  onClick={this.props.action}  >
+                <NavLink to={this.props.to} activeClassName={tabClassName}>{this.props.title}</NavLink>
+            </li>
+        );
+    };
+
+}
+
 
 class Sidebar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // Bind the this context to the handler function
+        this.handler = this.handler.bind(this);
+
+        this.state = {
+            activeItem: 'dashboard'
+        };
+
+
+    };
+
+    handler() {
+
+        this.setState({
+            activeItem: 'buttons'
+        });
+        
+        console.log('====================================');
+        console.log('handler', this);
+        console.log('====================================');
+    }
+
+
+
     render() {
         return (
             <div className="sidebar" data-active-color="blue" data-background-color="white" data-image="../assets/img/sephora2.png">
+
                 <div className="logo">
                     <a href="#" className="simple-text">
                         <img width="100px" src="https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/2575343/1162/776/m1/fpnw/wm0/1-.jpg?1492766875&s=b84d7135a1fbf8221a200ec727fd5e17" />
@@ -27,12 +97,10 @@ class Sidebar extends Component {
                         </div>
                         <div className="info">
                             <a data-toggle="collapse" href="#collapseExample" className="collapsed">
-                                Kim Stromberg
-                                    <b className="caret"></b>
+                                John Doe <b className="caret"></b>
                             </a>
                             <div className="collapse" id="collapseExample">
                                 <ul className="nav">
-
                                     <li>
                                         <a href="#">Access Panel</a>
                                     </li>
@@ -44,6 +112,7 @@ class Sidebar extends Component {
                         </div>
                     </div>
                     <ul className="nav">
+
                         <li>
                             <a data-toggle="collapse" href="#read">
                                 <i className="material-icons">timeline</i>
@@ -53,24 +122,13 @@ class Sidebar extends Component {
                             </a>
                             <div className="collapse in" id="read">
                                 <ul className="nav">
-                                    
-                                    <li className="active">
-                                        {/*<a href="dashboard.html">Insights</a>*/}
-                                        <NavLink to="/dashboard" activeClassName="selected">dashboard</NavLink>
-                                    </li>
-                                    <li>
-                                        {/*<a href="improving.html">Improving</a>*/}
-                                        <NavLink to="/buttons" activeClassName="selected">buttons</NavLink>
-                                    </li>
-                                    <li>
-                                        {/*<a href="improving.html">Improving</a>*/}
-                                        <NavLink to="/tables" activeClassName="selected">tables</NavLink>
-                                    </li>
-
-
+                                    <NavItem action={this.handler} activeItem={this.state.activeItem} itemId="dashboard" title="Dashboard" to="/" />
+                                    <NavItem action={this.handler} activeItem={this.state.activeItem} itemId="buttons"   title="Buttons" to="/buttons" />
+                                    <NavItem action={this.handler} activeItem={this.state.activeItem} itemId="tables"    title="Tables" to="/tables" />
                                 </ul>
                             </div>
                         </li>
+
                         <li>
                             <a data-toggle="collapse" href="#pagesExamples">
                                 <i className="material-icons">message</i>
@@ -93,6 +151,7 @@ class Sidebar extends Component {
                                 </ul>
                             </div>
                         </li>
+
                         <li>
                             <a data-toggle="collapse" href="#componentsExamples">
                                 <i className="material-icons">apps</i>
